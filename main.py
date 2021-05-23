e5ee97a40eb9b33a0a962d7ec85852c4a49121ae
royyi <>
2021-05-23 01:01:23 +0000

    added directions for project

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+# gomoku board
 # 12x12
+
+# player wins if they get 5 or more in a row
+
+# players take turns placing an X or an O piece
+# on an empty board
+
+# game class: manages the gameplay
+
+# player class: one player object for each player
+# get_move
+
+# board class: responsible for making a move on the board\
+# makes the board
+# gets all the available moves on the board (empty spaces)
+# checks if a player has won
+
+# PIECES ØX
