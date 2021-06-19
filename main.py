444cb2f4550dee6556511ec449d5980548d7a319
DanielMiao1 <>
2021-06-19 17:54:15 +0000

    added checks to detect if a player has won

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ class Game:
       self.board.board[row - 1][col - 1] = choices[int(turn)]
       turn = not turn
       player_won = self.board.has_player_won()
+    self.board.print_board()
     print(player_won)
 
 class Player:
@@ -63,21 +64,40 @@ class Board:
 
   def has_player_won(self):
     """Return the player if one won"""
-    for i in range(5):
-      if self.board[i][0] == self.board[i][1] == self.board[i][2] == self.board[i][3] == self.board[i][4] and self.board[i][0] != " ": return self.board[i][0]
-    # if any([True for i in range(5) if board[i][0] == board[i][1] == board[i][2] == board[i][3] == board[i][4] and board[i][0] != " "]):
-    # return (board[0][0] == board[1][1] == board[2][2] and board[0][0] != " ") or (board[0][2] == board[1][1] == board[2][0] and board[0][2] != " ") or any([True for i in range(5) if board[i][0] == board[i][1] == board[i][2] and board[i][0] != " "]) or any([True for i in range(3) if board[0][i] == board[1][i] == board[2][i] and board[0][i] != " "])
+    # check for horizontal wins
+    for x in self.board:
+      for y in range(len(x) - 4):
+        if x[y] == x[y + 1] == x[y + 2] == x[y + 3] == x[y + 4] and x[y] != " ": return x[y]
+    for x in range(len(x) - 4):
+      for y in range(12):
+        if self.board[x][y] == self.board[x + 1][y] == self.board[x + 2][y] == self.board[x + 3][y] == self.board[x + 4][y] and self.board[x][y] != " ": return self.board[x][y]
+    for x in range(len(self.board) - 4):
+      for y in range(8):
+        if self.board[x + 4][y + 4] == self.board[x + 3][y + 3] == self.board[x + 2][y + 2] == self.board[x + 1][y + 1] == self.board[x][y] and self.board[x][y] != " ": return self.board[x][y]
     return False
 
-  @staticmethod
-  def format_board(board): return "\n--+---+---+---+---+---+---+---+---+---+---+--\n".join(" | ".join(i) for i in board)
+  def format_board(self, board): return self.add_numbers("\n--+---+---+---+---+---+---+---+---+---+---+--\n".join(" | ".join(i) for i in board))
 
+  @staticmethod
+  def add_numbers(board):
+    new_board = ""
+    for x, y in zip(board.splitlines(), range(1, 26)):
+      if y % 2 == 0: new_board += "\n     " + x
+      else: new_board += f"\n{str((y // 2) + 1).ljust(3)} {x}"
+    return new_board
   def print_board(self):
     print(self.format_board(self.board))
-
-# i for in ["".join([str(row) for row in self.board])]
-
 
 
 g = Game()
 g.start_game()
+
+
+# check if there are 5 pieces in a row horizontally
+# check if there are 5 pieces in a row vertically
+
+# check all the diagonals for 5 in a row
+
+# refactor
+# if any(True for y in range(len([x for x in self.board]) - 4) if x[y] == x[y + 1] == x[y + 2] == x[y + 3] == x[y + 4] and x[y] != " ") or any()
+# if all(self.board[x][y=]  for y in )
